ad646b96fdf94142fb2202338715ba2b7d5996b9
Andriani <theblue_squirrel@yahoo.com>
2026-09-16 20:00:54 -0400

    Login Features
    - Fixed test for the title
     - Now checks for "Swag Labs" instead of "Labs Swag"
    - Fixed error message test not checking for an error message

--- playwrightUtilities.ts
+++ playwrightUtilities.ts
@@ -6,7 +6,9 @@ const DEFAULT_TIMEOUT = 30000;
 
 export const initializeBrowser = async () => {
   if (!browser) {
-    browser = await chromium.launch({ headless: false });
+    browser = await chromium.launch({ headless: false,
+      slowMo: 300 // N ms delay
+    });
   }
 };
 
